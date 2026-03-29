e820051d37bd32926445e5556d24f97550709bfa
omegaa01 <148469879+omegaa01@users.noreply.github.com>
2026-03-29 19:58:35 +0800

    Change title from 'Happy Girlfriends Day' to 'HAPPY BIRTHDAY Mi!!'

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girlfriends Day 💓').split('')
+    const titles = ('HAPPY BIRTHDAY Mi!!🥳').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
